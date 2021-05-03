51ab1fed1eb95dd38d833af4a6549bf4c539bb41
Garpepi <garpepi.aotearoa@gmail.com>
2021-05-04 00:56:42 +0700

    Target, border, and clear button

--- Pain Diary/Controllers/PainViewController.swift
+++ Pain Diary/Controllers/PainViewController.swift
@@ -9,46 +9,31 @@ import UIKit
 
 class PainViewController: UIViewController {
 
-  @IBOutlet weak var tempPoint: UIView!
   @IBOutlet weak var ClearButton: UIButton!
   @IBOutlet weak var SaveButton: UIButton!
   @IBOutlet weak var BodyPath: UIImageView!
   @IBOutlet weak var BodyDirection: UISegmentedControl!
-
-  @IBOutlet weak var locationCoor: UILabel!
-  @IBOutlet weak var colorTap: UILabel!
-
-  @IBOutlet weak var colorlocation: UILabel!
+  @IBOutlet weak var TrashTarget: UIImageView!
+  @IBOutlet weak var PainMeasurement: UISlider!
+  @IBOutlet weak var TouchArea: UIView!
+  
+  var seletedTarget:UIView = UIView()
 
   var frontPoint:[UIView] = []
   var backPoint:[UIView] = []
+
 
   override func viewDidLoad() {
       super.viewDidLoad()
 
         // Do any additional setup after loading the view.
       self.initView()
-
-    // TEST
-    print(BodyPath.layer.zPosition)
-    // EN DTEST
       self.BodyPath.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tappingHandler)))
-
-      self.tempPoint.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panningHandler)))
     }
     
   func initView(){
     self.ClearButton.layer.cornerRadius = 8
     self.SaveButton.layer.cornerRadius = 8
-  }
-
-  func tagIndex()-> Int{
-    if BodyDirection.selectedSegmentIndex == 0{
-      return self.frontPoint.count
-    }
-    else{
-      return self.backPoint.count
-    }
   }
 
   func createPainPoin(x: CGFloat, y:CGFloat){
@@ -63,45 +48,65 @@ class PainViewController: UIViewController {
     painPoint.tintColor = #colorLiteral(red: 0, green: 0.4176123142, blue: 0.6501051784, alpha: 1)
 
     // Put it in view
-    self.view.addSubview(painPoint)
-    self.view.bringSubviewToFront(painPoint)
+    self.TouchArea.addSubview(painPoint)
+    self.TouchArea.bringSubviewToFront(painPoint)
 
     // Adding Gesture on new UIView (painpoint)
     painPoint.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panningHandler))) // for dragging
     painPoint.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(selectedTapHandler))) // for selecting
 
     frontPoint.append(painPoint)
-    print(frontPoint)
     // Tag the UIView
-    painPoint.tag = tagIndex()
+    painPoint.tag = 15
+    borderSelected(gestureView: painPoint)
+  }
+
+  @IBAction func ClearTarget(_ sender: Any) {
+    if let foundView = self.TouchArea.viewWithTag(15) {
+        foundView.removeFromSuperview()
+    }
+
+    for subUIView in self.TouchArea.subviews as [UIView] {
+      if let foundView = subUIView.viewWithTag(15) {
+          foundView.removeFromSuperview()
+      }
+    }
 
   }
 
-
   @objc func tappingHandler(gesture: UITapGestureRecognizer){
-    print("CN tapped!")
-    let coordinate = gesture.location(in: self.view)
+    let coordinate = gesture.location(in: self.TouchArea)
     createPainPoin(x: coordinate.x - 16 , y: coordinate.y - 16)
     //print(BodyPath.getColourFromPoint(point: coordinate))
   }
 
   @objc func selectedTapHandler(gesture: UITapGestureRecognizer){
-    print("selected tapped!")
-    print(gesture.view)
+    // set border
+    // MARK ASK
+    borderSelected(gestureView: gesture.view!)
+  }
+
+  func borderSelected(gestureView: UIView){
+    if(!self.seletedTarget.frame.isEmpty){
+      self.seletedTarget.layer.borderWidth = 0
+    }
+
+    self.seletedTarget = gestureView
+    if(!gestureView.frame.isEmpty){
+      gestureView.layer.borderWidth = 1
+      gestureView.layer.borderColor = #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1)
+    }
+
   }
 
   @objc func panningHandler(gesture: UIPanGestureRecognizer){
-    //print(gesture)
-     // print(gesture.location(in: self.view))
-      let location = gesture.location(in: self.view)
-      //print("BodyPath : \(BodyPath.getColourFromPoint(point: location))")
-      let draggedView = gesture.view
-      draggedView?.center = location
-    // get x - y location of image
-    print("\(BodyPath.frame.origin.x) vs \(BodyPath.bounds.origin.x) ==> \(location.x + BodyPath.frame.origin.x)")
-    print("\(BodyPath.frame.origin.y) vs \(BodyPath.bounds.origin.y) ==> \(location.y + BodyPath.frame.origin.y)")
-    print(BodyPath.layer.colorOfPoint(point: CGPoint(x: location.x - BodyPath.frame.origin.x, y: location.y - BodyPath.frame.origin.y)))
-
+      borderSelected(gestureView: gesture.view!)
+      let location = gesture.location(in: self.TouchArea)
+      if((location.x > 0) && (location.x < self.TouchArea.frame.width) && (location.y > 0) && (location.y < self.TouchArea.frame.height)){
+        let draggedView = gesture.view
+        draggedView?.center = location
+        //print(self.BodyPath.layer.colorOfPoint(point: location))
+      }
   }
 
 }
@@ -113,20 +118,18 @@ extension CALayer {
         var pixel:[CUnsignedChar] = [0,0,0,0]
 
         let colorSpace = CGColorSpaceCreateDeviceRGB()
-      let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
 
-      let context = CGContext(data: &pixel, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace,bitmapInfo: bitmapInfo.rawValue)
+        let context = CGContext(data: &pixel, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace,bitmapInfo: bitmapInfo.rawValue)
 
-      context!.translateBy(x: -point.x, y: -point.y)
+        context!.translateBy(x: -point.x, y: -point.y)
 
-      self.render(in: context!)
+        self.render(in: context!)
 
         let red:CGFloat = CGFloat(pixel[0])/255.0
         let green:CGFloat = CGFloat(pixel[1])/255.0
         let blue:CGFloat = CGFloat(pixel[2])/255.0
         let alpha:CGFloat = CGFloat(pixel[3])/255.0
-
-        //println("point color - red:\(red) green:\(green) blue:\(blue)")
 
         let color = UIColor(red:red, green: green, blue:blue, alpha:alpha)
 
